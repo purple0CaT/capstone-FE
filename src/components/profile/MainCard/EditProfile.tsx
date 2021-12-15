ce3918c7620ed9f86ab1a75c6b9b9af5210a0495
Istvan <17463906+purple0CaT@users.noreply.github.com>
2021-12-15 23:07:53 +0000

    Fix

--- src/components/profile/MainCard/EditProfile.tsx
+++ src/components/profile/MainCard/EditProfile.tsx
@@ -1,19 +1,39 @@
 import SettingsIcon from "@mui/icons-material/Settings";
 import { LoadingButton } from "@mui/lab";
 import { Button, Dialog, Divider, IconButton, TextField } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function EditProfile({ userInfo, reFetch }: any) {
   const user = useSelector((state: any) => state.user);
   const tokens = useSelector((state: any) => state.tokens);
   const [OpenInfoUpdate, setOpenInfoUpdate] = useState(false);
-  const [FormInfo, setFormInfo] = useState(userInfo);
+  const [FormInfo, setFormInfo]: any = useState();
   const [Loading, setLoading] = useState(false);
+  const [NewLink, setNewLink] = useState({ title: "", link: "" });
   //
   const handleInfoUpdate = () => {
     setOpenInfoUpdate(!OpenInfoUpdate);
   };
+  //
+  const removeLink = (i: number) => {
+    if (FormInfo) {
+      const updateForm = FormInfo.links.slice(i, 1);
+      if (!updateForm.links) {
+        updateForm.links = [];
+      }
+      setFormInfo(updateForm);
+    }
+  };
+  const pushToForm = () => {
+    if (FormInfo) {
+      const updateForm = FormInfo;
+      updateForm.links.push(NewLink);
+      setFormInfo(updateForm);
+    }
+  };
+  //
   const updateUserInfo = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
@@ -39,6 +59,9 @@ function EditProfile({ userInfo, reFetch }: any) {
       console.log(error);
     }
   };
+  useEffect(() => {
+    setFormInfo(userInfo);
+  }, []);
   return (
     <>
       <IconButton onClick={handleInfoUpdate}>
@@ -47,12 +70,11 @@ function EditProfile({ userInfo, reFetch }: any) {
       <Dialog
         open={OpenInfoUpdate}
         //   TransitionComponent={Transition}
-        keepMounted
+        // keepMounted
         onClose={handleInfoUpdate}
-        aria-describedby="alert-dialog-slide-description"
-        sx={{
-          backdropFilter: "blur(2px)",
-        }}
+        // sx={{
+        //   backdropFilter: "blur(2px)",
+        // }}
       >
         <h6 className="text-center mt-3">Update profile</h6>
         {FormInfo && (
@@ -64,7 +86,7 @@ function EditProfile({ userInfo, reFetch }: any) {
               className="mr-2"
               required
               id="standard-required"
-              label="NickName"
+              // label="NickName"
               margin="dense"
               variant="standard"
               value={FormInfo.nickname}
@@ -110,6 +132,63 @@ function EditProfile({ userInfo, reFetch }: any) {
                 setFormInfo({ ...FormInfo, bio: e.target.value })
               }
             />
+            <br />
+            {/* <div className="d-flex flex-column align-items-center mt-2">
+              <h5 className="text-muted mr-auto">Links:</h5>
+              {FormInfo.links?.length > 0 &&
+                FormInfo.links.map((Li: any, i: number) => (
+                  <div
+                    key={Li.link + i + "a4"}
+                    className="d-flex justify-content-between align-items-center"
+                  >
+                    <span className="d-flex align-items-baseline">
+                      {Li.title}:{" "}
+                      <h6 className="m-0 ml-2 font-weight-light">{Li.link}</h6>
+                    </span>
+                    <IconButton color="warning" onClick={() => removeLink(i)}>
+                      <DeleteIcon fontSize="small" />
+                    </IconButton>
+                  </div>
+                ))}
+            </div> */}
+            {/* <div className="d-flex flex-column align-items-center">
+              <div>
+                <TextField
+                  className="mr-2"
+                  required
+                  id="newLink"
+                  label="New Title"
+                  margin="dense"
+                  variant="standard"
+                  value={NewLink.title}
+                  onChange={(e: any) =>
+                    setNewLink({ ...NewLink, title: e.target.value })
+                  }
+                />
+                <TextField
+                  className="mr-2"
+                  required
+                  id="newLink"
+                  label="New Link"
+                  margin="dense"
+                  variant="standard"
+                  value={NewLink.link}
+                  onChange={(e: any) =>
+                    setNewLink({ ...NewLink, link: e.target.value })
+                  }
+                />
+              </div>
+              <Button
+                disabled={
+                  NewLink.link.length > 0 && NewLink.title.length > 0
+                    ? false
+                    : true
+                }
+                onClick={() => pushToForm()}
+              >
+                Add to links
+              </Button>
+            </div> */}
             <br />
             <Divider />
             {/* {Loading && <LinearProgress />} */}
--- src/components/profile/MainCard/MainCard.tsx
+++ src/components/profile/MainCard/MainCard.tsx
@@ -24,6 +24,7 @@ function MainCard({ FetchedUser, reFetch, FetchedCreator }: any) {
   //
   const [OpenFollowers, setOpenFollowers] = useState(false);
   const [OpenFollowing, setOpenFollowing] = useState(false);
+  const [OpenLinks, setOpenLinks] = useState(false);
   const [Following, setFollowing] = useState([]);
   const [Followers, setFollowers] = useState([]);
   //
@@ -111,7 +112,7 @@ function MainCard({ FetchedUser, reFetch, FetchedCreator }: any) {
           </div>
         </div>
       </Grid>
-      {/* USER INFORMATION */}
+      {/*  =============================================================================  USER INFORMATION */}
       <Grid item xs={12} sm={8}>
         <div
           className="user-info"
@@ -140,13 +141,7 @@ function MainCard({ FetchedUser, reFetch, FetchedCreator }: any) {
               </>
             )}
           </div>
-          <br />
-          {/* NAME BIO  */}
-          <div className="mr-auto">
-            <h4 className="font-weight-light">
-              {FetchedUser.user.firstname + " " + FetchedUser.user.lastname}
-            </h4>
-          </div>
+          {/*  ============================================================================= NAME BIO  */}
           {FetchedCreator && (
             <div>
               <small className="font-weight-bold text-muted">
@@ -155,12 +150,17 @@ function MainCard({ FetchedUser, reFetch, FetchedCreator }: any) {
             </div>
           )}
           <div className="mr-auto">
+            <h4 className="font-weight-light">
+              {FetchedUser.user.firstname + " " + FetchedUser.user.lastname}
+            </h4>
+          </div>
+
+          <div className="mr-auto">
             <p className="m-0">{FetchedUser.user.bio}</p>
           </div>
-          <br />
-          {/* FOLLOWERS */}
+          {/* ============================================================================= FOLLOWERS */}
           <div
-            className="d-flex w-100 align-items-center justify-content-center"
+            className="d-flex w-100 align-items-center justify-content-center mt-2"
             style={{ gap: "2rem" }}
           >
             <Button
@@ -227,7 +227,6 @@ function MainCard({ FetchedUser, reFetch, FetchedCreator }: any) {
               TransitionComponent={Transition}
               keepMounted
               onClose={handleFollowing}
-              aria-describedby="alert-dialog-slide-description"
               sx={{
                 backdropFilter: "blur(2px)",
               }}
@@ -268,6 +267,37 @@ function MainCard({ FetchedUser, reFetch, FetchedCreator }: any) {
               </div>
             </Dialog>
           </div>
+          <div className="mt-2">
+            <Button
+              size="small"
+              color="info"
+              onClick={() => setOpenLinks(true)}
+            >
+              {" "}
+              Contact links
+            </Button>
+            <Dialog open={OpenLinks} onClose={() => setOpenLinks(false)}>
+              <div className="p-3 d-flex flex-column align-items-center">
+                <span className="d-flex align-items-baseline">
+                  Email:{" "}
+                  <h5 className="m-0 ml-2 font-weight-light">
+                    {FetchedUser.user.email}
+                  </h5>
+                </span>
+                {FetchedUser.user.links.length > 0 &&
+                  FetchedUser.user.links.map((Li: any) => (
+                    <a
+                      target="_blank"
+                      href={Li.link}
+                      key={Li + "z13"}
+                      className="my-1"
+                    >
+                      <h5 className="m-0 ml-2 font-weight-light">{Li.title}</h5>
+                    </a>
+                  ))}
+              </div>
+            </Dialog>
+          </div>
         </div>
       </Grid>
     </div>
@@ -275,162 +305,3 @@ function MainCard({ FetchedUser, reFetch, FetchedCreator }: any) {
 }
 
 export default MainCard;
-
-// // <div className="profile-page">
-// <div className="position-relative h-100 col-12 col-md-4 h-100 p-0">
-// <div className="position-relative ">
-//   <UpdateBG user={FetchedUser.user} reFetch={reFetch} />
-//   <div className="img-avatar" style={{ width: "8rem", height: "8rem" }}>
-//     <UpdateAvatar user={FetchedUser.user} reFetch={reFetch} />
-//   </div>
-// </div>
-// </div>
-// {/* USER INFORMATION */}
-// <div className="user-info col-12 col-md-8">
-// <div className="d-flex justify-content-between align-items-center w-100">
-//   <h4 className="font-weight-light mr-auto m-0">
-//     {FetchedUser.user.nickname}
-//   </h4>
-//   {user._id === FetchedUser.user._id ? (
-//     <EditProfile userInfo={FetchedUser.user} reFetch={reFetch} />
-//   ) : (
-//     <>
-//       {FetchedUser.followers.followers.some(
-//         (F: any) => F._id == user._id,
-//       ) ? (
-//         <Button color="warning" onClick={() => unfollowUser()}>
-//           Unfollow
-//         </Button>
-//       ) : (
-//         <Button onClick={() => followUser()}>Follow</Button>
-//       )}
-//     </>
-//   )}
-// </div>
-// <br />
-// {/* NAME BIO  */}
-// <div className="mr-auto">
-//   <h5 className="font-weight-light">
-//     {FetchedUser.user.firstname + " " + FetchedUser.user.lastname}
-//   </h5>
-// </div>
-// {FetchedCreator && (
-//   <div>
-//     <small className="font-weight-bold text-muted">
-//       {FetchedCreator.creatorType}
-//     </small>
-//   </div>
-// )}
-// <div className="mr-auto">
-//   <p className="m-0">{FetchedUser.user.bio}</p>
-// </div>
-// <br />
-// {/* FOLLOWERS */}
-// <div
-//   className="d-flex w-100 align-items-center"
-//   style={{ gap: "2rem" }}
-// >
-//   <Button
-//     disabled={Followers.length > 0 ? false : true}
-//     color="primary"
-//     onClick={handleFollowers}
-//   >
-//     {" "}
-//     <h6 className="font-weight-light m-0">
-//       Followers {Followers.length}
-//     </h6>
-//   </Button>
-//   <Button
-//     disabled={Following.length > 0 ? false : true}
-//     color="info"
-//     onClick={handleFollowing}
-//   >
-//     {" "}
-//     <h6 className="font-weight-light m-0">
-//       Following {Following.length}
-//     </h6>
-//   </Button>
-// </div>
-// {/* Dialogs */}
-// <div>
-//   <Dialog
-//     open={OpenFollowers}
-//     TransitionComponent={Transition}
-//     keepMounted
-//     onClose={handleFollowers}
-//     aria-describedby="alert-dialog-slide-description"
-//     sx={{
-//       backdropFilter: "blur(2px)",
-//     }}
-//   >
-//     <div className="d-flex flex-column p-3">
-//       {Followers.length > 0 &&
-//         Followers.map((F: any) => (
-//           <div
-//             className="d-flex align-items-center justify-content-between p-1"
-//             key={F._id + 131}
-//           >
-//             <Link
-//               to={`/profile/${F._id}`}
-//               className="d-flex align-items-center"
-//               style={{ minWidth: "13rem" }}
-//             >
-//               <Avatar
-//                 alt={F.firstname + " " + F.lastname}
-//                 src={F.avatar}
-//                 sx={{ width: "2rem", height: "2rem" }}
-//               />
-//               <p className="m-0 ml-2">
-//                 {F.firstname + " " + F.lastname}
-//               </p>
-//             </Link>
-//             <div></div>
-//           </div>
-//         ))}
-//     </div>
-//   </Dialog>
-//   <Dialog
-//     open={OpenFollowing}
-//     TransitionComponent={Transition}
-//     keepMounted
-//     onClose={handleFollowing}
-//     aria-describedby="alert-dialog-slide-description"
-//     sx={{
-//       backdropFilter: "blur(2px)",
-//     }}
-//   >
-//     <div className="d-flex flex-column p-3">
-//       {Following.length > 0 &&
-//         Following.map((F: any) => (
-//           <div
-//             className="d-flex align-items-center justify-content-between p-1"
-//             key={F._id + 321}
-//           >
-//             <Link
-//               to={`/profile/${F._id}`}
-//               className="d-flex align-items-center"
-//               style={{ minWidth: "13rem" }}
-//             >
-//               <Avatar
-//                 alt={F.firstname + " " + F.lastname}
-//                 src={F.avatar}
-//                 sx={{ width: "2rem", height: "2rem" }}
-//               />
-//               <p className="m-0 ml-2">
-//                 {F.firstname + " " + F.lastname}
-//               </p>
-//             </Link>
-//             {FetchedUser.user._id === user._id && (
-//               <div>
-//                 <Button color="warning" onClick={() => unFollow(F._id)}>
-//                   Unfollow
-//                 </Button>
-//               </div>
-//             )}
-//           </div>
-//         ))}
-//     </div>
-//   </Dialog>
-// </div>
-// </div>
-// </div>
